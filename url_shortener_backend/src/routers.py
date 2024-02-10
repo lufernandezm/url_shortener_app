3133c511ea055eddea85337da9601c512f3f5f03
Luisa Madrid <femalufe94@gmail.com>
2024-02-09 22:47:07 -0500

    erro handling implementation

--- url_shortener_backend/src/routers.py
+++ url_shortener_backend/src/routers.py
@@ -3,6 +3,7 @@ from fastapi.responses import RedirectResponse, JSONResponse
 from models import Url
 import string
 import random
+from urllib.parse import urlparse
 
 router = APIRouter()
 
@@ -21,18 +22,28 @@ def generate_short_url():
         short_url = ''.join(random.choices(string.ascii_uppercase + string.digits, k=LENGTH_SHORT_URL))
         if short_url not in url_mapping:
             return short_url
+        
+def is_valid_url(url):
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
 
 @router.post("/shorten")
 def create_short_url(url: Url):
+    if not is_valid_url(url.url):
+        raise HTTPException(status_code=400, detail="Invalid URL provided.")
     try:
         short_url = find_url(url_mapping, url.url)
-        print(url_mapping)
         if not short_url:
             short_url = generate_short_url()
             url_mapping[short_url] = {'url': url.url, 'visits': 0}
         return JSONResponse(status_code=200, content={"shortUrl": f"{BASE_URL}/{short_url}"})
+    except KeyError as e:
+        raise HTTPException(status_code=400, detail="Invalid URL provided.")
     except Exception as e:
-        return JSONResponse(status_code=400, content={"message": str(e)})
+        raise HTTPException(status_code=500, detail="An unexpected error occurred.")
 
 
 @router.get("/{short_url}")
@@ -41,6 +52,7 @@ def redirect(short_url: str):
         if short_url not in url_mapping:
             raise HTTPException(status_code=404, detail="Short URL not found")
         url_mapping[short_url]['visits'] += 1
+        print(url_mapping)
         return RedirectResponse(url=url_mapping[short_url]['url'])
     except HTTPException as e:
         return JSONResponse(status_code=e.status_code, content={"message": e.detail})
